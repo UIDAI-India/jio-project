4d0afab5fbe2fa9386269d1ba9ef30e620960bb3
MdNajim8171 <najimsiet.001gmail.com>
2025-06-29 21:02:31 +0530

    one more rg added

--- azurerm-resource-group/main.tf
+++ azurerm-resource-group/main.tf
@@ -2,3 +2,8 @@ resource "azurerm_resource_group" "rg01" {
   name     = var.rg-name
   location = var.rg-location
 }
+
+resource "azurerm_resource_group" "rg02" {
+  name     = "rg-najim"
+  location = "eastus"
+}
