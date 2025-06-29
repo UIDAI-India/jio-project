7f6a56d2a07a8fdfdf47f7817300820d2a87cd4a
MdNajim8171 <najimsiet.001gmail.com>
2025-06-29 20:55:09 +0530

    RG has been added

---
+++ azurerm-resource-group/variable.tf
@@ -0,0 +1,12 @@
+variable "rg-name" {
+  description = "The name of the resource group."
+  type        = string
+  
+  
+}
+variable "rg-location" {
+  description = "The Azure region where the resource group will be created."
+  type        = string
+ 
+  
+}
